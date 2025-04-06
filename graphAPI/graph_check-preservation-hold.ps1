9f7612bb536dacd516f719388ae8e1d0abc5e356
panoy86 <panoy@rocketmail.com>
2025-04-06 12:50:22 -0400

    Update graph_check-preservation-hold.ps1

--- graphAPI/graph_check-preservation-hold.ps1
+++ graphAPI/graph_check-preservation-hold.ps1
@@ -10,9 +10,9 @@ function udf_AuthGraphApi
         [Parameter()]$bSilent = $true
     )
 
-    $sTenantId = "fb007914-6020-4374-977e-21bac5f3f4c8"
-    $sClientId = "2417f530-7fe2-48ef-85c7-d5f09b9564d7"
-    $sSecretB64 = "OWx4OFF+MGlrSnNabVVUYS1wNXZXfjVTS0hqVE9lY3ozX29BaGNqTQ=="
+    $sTenantId = ""
+    $sClientId = ""
+    $sSecretB64 = ""
     $bytes = [System.Convert]::FromBase64String($sSecretB64)
     $sClientSecret = [System.Text.Encoding]::UTF8.GetString($bytes)
 
