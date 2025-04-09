1dc4cc343dede8d1d1a66fcdb4914ae5f79eab72
panoy86 <panoy@rocketmail.com>
2025-04-09 11:33:56 -0400

    Update Get-DLMembers.ps1

--- Get-DLMembers.ps1
+++ Get-DLMembers.ps1
@@ -1,84 +1,104 @@
 #-- Script to extract all members of a DL, and members of sub-DLS, etc...
 $rDLs = @()
-$rDls += "something"
+$rDls += "some-DL"
 
 #-- Main program, do not change
-$global:hTotalMembers = @{}
-$global:nTotalDLCount = 0
-$global:hGroupsFound = @{}  #-- This is used to detect loops
-$global:rSubDLs = @()
+$script:hTotalMembers = @{}
+$script:nTotalDLCount = 0
+$script:hGroupsFound = @{}  #-- This is used to detect loops
+$script:rSubDLs = @()
 
-
+#---------------------------------------------------------------------------------
 #-- Recursive function for DL search
-function udfGet-DLMembers([String] $sDL)
+#---------------------------------------------------------------------------------
+function Get-DLMembersRecursive
 {
+    param([String] $sDL)
+    
     #-- Find the DL
-    $d = $null
-    $d = Get-DistributionGroup -Identity $sDL -ea SilentlyContinue
-    if ($d -ne $null)
+    $oDL = $null
+    $oDL = Get-DistributionGroup -Identity $sDL -ea SilentlyContinue
+    if ($null -ne $oDL)
     {
         #-- Check for loop
-        if ($global:hGroupsFound.ContainsKey($d.Guid.ToString()))
+        if ($script:hGroupsFound.ContainsKey($oDL.Guid.ToString()))
         {
-            Write-Host ($sSpacePrefix + $d.Name.ToString() + " (loop found)") -ForegroundColor Green
+            Write-Host ($oDL.Name.ToString() + " (loop found)") -ForegroundColor Green
         }
         else
         {
             #-- Get the members
-            $global:hGroupsFound.Add($d.Guid.ToString(), 1)
-            [array]$rMembers = Get-DistributionGroupMember $d.Guid.ToString() -ResultSize unlimited
-            
-            #-- Show DL info
-            #if ($r.Count -ge 500) {Write-Host $d.DisplayName $r.Count -ForegroundColor Yellow}
-            #else {Write-Host $d.DisplayName $r.Count}
+            $script:hGroupsFound.Add($oDL.Guid.ToString(), 1)
+            [array]$rMembers = Get-DistributionGroupMember $oDL.Guid.ToString() -ResultSize unlimited
             
             #-- Show progress
-            Write-Host ($sSpacePrefix + $d.Name + ' ' + $rMembers.Count.ToString())
+            Write-Host ($oDL.Name + ' ' + $rMembers.Count.ToString())
 
             #-- Loop thru members
             if ($rMembers.Count -gt 0)
             {
                 #-- Track the sub-DLs
-                $global:rSubDLs += ($rMembers.Count.ToString() + ", " + $d.Name)
-                Write-Progress ($rMembers.Count.ToString() + ", " + $d.Name) -Id 2
-                foreach($t in $rMembers)
+                $script:rSubDLs += ($rMembers.Count.ToString() + ", " + $oDL.Name)
+                Write-Progress -Activity "Finding member DLs" -Status ($rMembers.Count.ToString() + ", " + $oDL.Name)
+                foreach($oMember in $rMembers)
                 {
                     #-- Recursively call if member is another group
-                    if ($t.RecipientType -like "*Group")
+                    if ($oMember.RecipientType -like "*Group")
                     {
-                        $global:nTotalDLCount++
-                        udfGet-DLMembers($t.PrimarySmtpAddress.ToString())
+                        $script:nTotalDLCount++
+                        Get-DLMembersRecursive($oMember.PrimarySmtpAddress.ToString())
                     }
                     #-- Add user-mailbox/mail-contact to list
                     else
                     {
-                        $sEmail = $t.PrimarySmtpAddress.ToString().ToLower().Trim()
-                        if (-not $hMembers.ContainsKey($sEmail)) {$hMembers.Add($sEmail, $d.Name)}
-                        else {Write-Host "   duplicate:" $sEmail $hMembers.Get_Item($sEmail) -ForegroundColor Red} 
-                        if (-not $global:hTotalMembers.ContainsKey($sEmail))
-                        { $global:hTotalMembers.Add($sEmail, $t) }
+                        $sEmail = $oMember.PrimarySmtpAddress.ToString().ToLower().Trim()
+                        if (-not $script:hMembers.ContainsKey($sEmail)) {$script:hMembers.Add($sEmail, $oDL.Name)}
+                        else
+                        {
+                            Write-Host "   duplicate: " -ForegroundColor Yellow -NoNewline
+                            Write-Host $sEmail $script:hMembers[$sEmail]
+                        }
+                        if (-not $script:hTotalMembers.ContainsKey($sEmail))
+                        {$script:hTotalMembers.Add($sEmail, $oMember)}
                     }
                 }
             }
         }
     }
+    Write-Progress -Activity "Finding member DLs" -Completed
 }
 
+#------------------------------------------------------------------------------
+#-- Main
+#------------------------------------------------------------------------------
+$PSStyle.Progress.View = "Minimal"  #-- Other values: "Classic"
+$ProgressPreference = "Continue"
 
-#-- Loop thru list of DLs
-for($i=0; $i -lt $rDLs.Count; $i++)
+#-- Loop thru the DLs
+foreach ($sDL in $rDLs)
 {
-    $sDL = $rDLs[ $i ]
-    $hMembers = @{}
-    udfGet-DLMembers($sDL)
-    Write-Host ($sDL  + " (" + $hMembers.Count + ")")
+    $script:hMembers = @{}
+    Get-DLMembersRecursive($sDL)
+    Write-Host ($sDL  + " (" + $script:hMembers.Count + ")")
 }
-Write-Host "Total users:" $global:hTotalMembers.Count
-Write-Host "Total DLs (duplicates allowed):" $global:nTotalDLCount
-$global:hTotalMembers[0] | fl
+Write-Host "Total users:" $script:hTotalMembers.Count
+Write-Host "Total DLs (duplicates allowed):" $script:nTotalDLCount
+
+#-- Optional, get the SMTP list only
+$rFinal = @()
+$rTmp = $script:hTotalMembers.Get_Keys()
+foreach($t in $rTmp)
+{
+    if ($script:hTotalMembers.ContainsKey($t)) {$rFinal += $script:hTotalMembers[$t]}
+}
+$rTmp = @()
+$rFinal | ForEach-Object {$rTmp += $_.PrimarySmtpAddress}
+$rTmp | Out-File -Encoding ASCII .\emails-list.txt
+Write-Host "File created: emails-list.txt"
+Write-Host "Members:" $rFinal.Count
 
 <#-- Fix all-upper-case names
-$r1 = @(); $hMembers.GetEnumerator()| foreach {$r1 += $_.Value}
+$r1 = @(); $script:hMembers.GetEnumerator()| foreach {$r1 += $_.Value}
 $TextInfo = (Get-Culture).TextInfo
 foreach($t in $r1)
 {
@@ -88,116 +108,4 @@ foreach($t in $r1)
     #if ($sTmp.Length -gt 1 -and $sTmp -cmatch "^[A-Z]*$") {$t.FirstName = $TextInfo.ToTitleCase($sTmp.ToLower())}
     #if ($sTmp.Length -gt 1 -and $sTmp -cmatch "^[A-Z]*$") {$t.LastName = $TextInfo.ToTitleCase($sTmp.ToLower())}
 }
-#>
-
-#-- Get the SMTP list only
-$rFinal = @()
-$rTmp = $global:hTotalMembers.Get_Keys()
-foreach($t in $rTmp)
-{
-    if ($global:hTotalMembers.ContainsKey($t)) { $rFinal += $global:hTotalMembers.Get_Item($t) }
-}
-$rTmp = @()
-$rFinal | foreach {$rTmp += $_.PrimarySmtpAddress}
-$rTmp | Out-File -Encoding ASCII .\emails-list.txt
-Write-Host "File created: emails-list.txt"
-Write-Host "Members:" $rFinal.Count
-<#
-$global:rSubDLs | Out-File .\Get-DLMembers_Sub-DLs.txt
-Write-Host "SubDL count: Get-DLMembers_Sub-DLs.txt"
-#>
-
-<#
-#-- Transfer to an array
-$rFinal = @()
-$rTmp = $global:hTotalMembers.Get_Keys()
-foreach($t in $rTmp)
-{
-    if ($global:hTotalMembers.ContainsKey($t)) { $rFinal += $global:hTotalMembers.Get_Item($t) }
-}
-$rFinal | select SamAccountName,Name,PrimarySmtpAddress,Database,ServerName | Sort Database | Export-Csv zz.csv
-#-- OR
-
-$nCtr = 0
-$rExport = @()
-foreach($t in $rFinal)
-{
-    $nCtr++; Write-Progress -Activity '.' -Status $t.Name -PercentComplete ($nCtr/$rFinal.Count * 100)
-    $oNew = New-Object System.Object
-    $oNew | Add-Member –Type NoteProperty –Name SamAccountName –Value $t.SamAccountName
-    $oNew | Add-Member –Type NoteProperty –Name Name –Value $t.Name
-    $oNew | Add-Member –Type NoteProperty –Name PrimarySmtpAddress –Value $t.PrimarySmtpAddress.ToString()
-    $oNew | Add-Member –Type NoteProperty –Name Database –Value $t.Database
-    $oNew | Add-Member –Type NoteProperty –Name ServerName –Value $t.ServerName
-    $sEmailAddresses = ""
-    foreach($oEmail in $t.EmailAddresses)
-    {
-        if ($oEmail.PrefixString.ToLower() -eq "smtp") { $sEmailAddresses += ($oEmail.AddressString + "; ") }
-    }
-    $oNew | Add-Member –Type NoteProperty –Name EmailAddresses –Value $sEmailAddresses
-    $rExport += $oNew
-}
-$rExport | Sort Database | Export-Csv zz.csv
-#>
-<#
-class myDL
-{
-    #-- Properties
-    [string] $sDLName
-    [array] $rMembers
-    $oDL
-    $hTmp
-    
-    #-- Init
-    myDL([string] $sDLIdentity)
-    {
-        $this.oDL = $null
-        $this.oDL = Get-DistributionGroup $sDLIdentity -ea SilentlyContinue
-        $this.hTmp = @{}
-    }
-    
-    #-- Method, get members of DL recursively (to be called internally only)
-    GetDLMembersRecursively([string] $sDLEmail)
-    {
-        Write-Host $sDLEmail
-        $d = $null
-        $d = Get-DistributionGroup -Identity $sDLEmail -ea SilentlyContinue
-        if ($d -ne $null)
-        {
-            [array]$rTmp = Get-DistributionGroupMember $sDLEmail -Resultsize unlimited
-            if ($rTmp.Count -gt 0)
-            {
-                foreach($oMember in $rTmp)
-                {
-                    if ($oMember.RecipientType -like "*Group")
-                    {
-                        $this.GetDLMembersRecursively($oMember.PrimarySmtpAddress.ToString())
-                    }
-                    else
-                    {
-                        $sKey = $oMember.PrimarySmtpAddress.ToString()
-                        if (-not $this.hTmp.ContainsKey($sKey))
-                        {
-                            $this.hTmp.Add($sKey, 1)
-                            $this.rMembers += $oMember
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    #-- Method, get members of DL
-    GetMembers()
-    {
-        $this.rMembers = @()
-        $this.GetDLMembersRecursively($this.oDL.PrimarySmtpAddress.ToString())
-    }
-}
-
-$cDL = [myDL]::new("dl-pp-gco")
-$cDL.GetMembers()
-$cDL.rMembers | select PrimarySmtpAddress | Out-File .\zz.txt
-Write-Host "Found" $cDL.rMembers.Count "(zz.txt)"
-Write-Host "   $cDL.GetMembers - has the members list"
 #>
