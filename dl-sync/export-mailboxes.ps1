6c44689fc2f507dde0d5318862759ec6fb981e8e
panoy86 <panoy@rocketmail.com>
2025-04-30 11:08:59 -0400

    Update export-mailboxes.ps1

--- dl-sync/export-mailboxes.ps1
+++ dl-sync/export-mailboxes.ps1
@@ -3,7 +3,7 @@ Write-Host "Getting all mailboxes, please wait..." -NoNewLine
 $rm = Get-Mailbox -ResultSize 10000; $rm.Count
 
 #-- Normalize certain properties
-$rm | foreach {$_ | Add-Member -Type NoteProperty -Name "EmailAddressesString" -Value ($_.EmailAddresses -join ';') -Force}
+$rm | ForEach-Object {$_ | Add-Member -Type NoteProperty -Name "EmailAddressesString" -Value ($_.EmailAddresses -join ';') -Force}
 
 #-- Save the export
-$rm | select Alias,EmailAddressesString,Guid,LegacyExchangeDN,PrimarySmtpAddress,RecipientTypeDetails | Export-Csv exp-mbx.csv -NoTypeInformation
+$rm | Select-Object Alias,DisplayName,Name,EmailAddressesString,Guid,LegacyExchangeDN,PrimarySmtpAddress,RecipientTypeDetails,OrganizationalUnit | Export-Csv exp-mbx.csv -NoTypeInformation
